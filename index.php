7389fd739bb37f94d6bdc510a5e581ee5f944dca
buhho <ajhernand@hotmail.com>
2021-02-23 13:49:18 -0600

    Subiendo archivos de prueba desde Laptop Dell, folder Test01

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+    <h1>Pagina de Pruebas</h1>
+    <h3>Version 23/Feb/2021</h3>
+    <?php
+
+    include("modulo1.php");
+    echo "Texto en PHP.  ";
+    echo "Funcion de suma:  a+b = ".suma(4,3);
+    ?>   
+    </div>
+
+</body>
+</html>
+
+
